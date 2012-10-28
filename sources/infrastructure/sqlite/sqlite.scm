ff599183a2d73f40bd6dbcb13469b9b4c9d0d936
Mathieu Desrochers <mathieu@desrochers>
2012-10-28 16:25:14 -0400

    Sqlite FFI - First draft at bound parameter values

--- sources/infrastructure/sqlite/sqlite.scm
+++ sources/infrastructure/sqlite/sqlite.scm
@@ -1,6 +1,4 @@
 
-(use srfi-1)
-
 (foreign-declare "
 
 #include <sqlite3.h>
@@ -85,6 +83,12 @@ void free_sqlite3_stmt_pointer(sqlite3_stmt** sqlite3_stmt_pointer)
 (define sqlite3-column-double (foreign-lambda double "sqlite3_column_double" sqlite3-stmt* int))
 (define sqlite3-column-text (foreign-lambda unsigned-c-string "sqlite3_column_text" sqlite3-stmt* int))
 
+; binds the value of the parameter at the specified index
+(define sqlite3-bind-int (foreign-lambda int "sqlite3_bind_int" sqlite3-stmt* int int))
+(define sqlite3-bind-double (foreign-lambda int "sqlite3_bind_double" sqlite3-stmt* int double))
+;(define sqlite3-bind-text (foreign-lambda int "sqlite3_bind_text" sqlite3-stmt* int (const c-string) int ))
+(define sqlite3-bind-null (foreign-lambda int "sqlite3_bind_null" sqlite3-stmt* int))
+
 ; finalizes a sql statement
 (define sqlite3-finalize (foreign-lambda int "sqlite3_finalize" sqlite3-stmt*))
 
@@ -93,60 +97,103 @@ void free_sqlite3_stmt_pointer(sqlite3_stmt** sqlite3_stmt_pointer)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use srfi-1)
+
 ; open the database
-(define sqlite3** (malloc-sqlite3*))
-(display "sqlite3-open: ")
-(display (sqlite3-open "customers.db" sqlite3**))
-(display "\n")
+(let ((sqlite3** (malloc-sqlite3*)))
 
-; prepare the sql statement
-(define sqlite3-stmt** (malloc-sqlite3-stmt*))
-(display "sqlite3-prepare-v2: ")
-(display (sqlite3-prepare-v2 (indirect-sqlite3** sqlite3**) "SELECT 1, 1.2, 'A', NULL;" -1 sqlite3-stmt** #f))
-(display "\n")
+  (display "sqlite3-open: ")
+  (display (sqlite3-open "customers.db" sqlite3**))
+  (display "\n")
 
-; execute the sql statement
-(display "sqlite3-step: ")
-(display (sqlite3-step (indirect-sqlite3-stmt** sqlite3-stmt**)))
-(display "\n")
+  ; create the customers table
+  (let ((sqlite3-stmt** (malloc-sqlite3-stmt*)))
 
-; get the number of returned columns
-(display "sqlite3-column-count: ")
-(define columns-count (sqlite3-column-count (indirect-sqlite3-stmt** sqlite3-stmt**)))
-(display columns-count)
-(display "\n")
+    ; prepare the sql statement
+    (display "sqlite3-prepare-v2: ")
+    (display (sqlite3-prepare-v2 (indirect-sqlite3** sqlite3**) "CREATE TABLE IF NOT EXISTS Customers (CustomerID INT, Age REAL, Name TEXT);" -1 sqlite3-stmt** #f))
+    (display "\n")
 
-; get the types of the columns
-(map
-  (lambda (column-index)
-    (display "sqlite3-column-type: ")
-    (display (sqlite3-column-type (indirect-sqlite3-stmt** sqlite3-stmt**) column-index))
+    ; execute the sql statement
+    (display "sqlite3-step: ")
+    (display (sqlite3-step (indirect-sqlite3-stmt** sqlite3-stmt**)))
+    (display "\n")
+
+    ; finalize the sql statement
+    (display "sqlite3-finalize: ")
+    (display (sqlite3-finalize (indirect-sqlite3-stmt** sqlite3-stmt**)))
+    (free-sqlite3-stmt* sqlite3-stmt**)
     (display "\n"))
-  (iota columns-count))
 
-; get the value of the integer column
-(display "sqlite3-column-int: ")
-(display (sqlite3-column-int (indirect-sqlite3-stmt** sqlite3-stmt**) 0))
-(display "\n")
+  ; insert a customer row
+  (let ((sqlite3-stmt** (malloc-sqlite3-stmt*)))
 
-; get the value of the double column
-(display "sqlite3-column-double: ")
-(display (sqlite3-column-double (indirect-sqlite3-stmt** sqlite3-stmt**) 1))
-(display "\n")
+    ; prepare the sql statement
+    (display "sqlite3-prepare-v2: ")
+    (display (sqlite3-prepare-v2 (indirect-sqlite3** sqlite3**) "INSERT INTO Customers VALUES (1, 1.2, 'A');" -1 sqlite3-stmt** #f))
+    (display "\n")
 
-; get the value of the text column
-(display "sqlite3-column-text: ")
-(display (sqlite3-column-text (indirect-sqlite3-stmt** sqlite3-stmt**) 2))
-(display "\n")
+    ; execute the sql statement
+    (display "sqlite3-step: ")
+    (display (sqlite3-step (indirect-sqlite3-stmt** sqlite3-stmt**)))
+    (display "\n")
 
-; finalize the sql statement
-(display "sqlite3-finalize: ")
-(display (sqlite3-finalize (indirect-sqlite3-stmt** sqlite3-stmt**)))
-(free-sqlite3-stmt* sqlite3-stmt**)
-(display "\n")
+    ; finalize the sql statement
+    (display "sqlite3-finalize: ")
+    (display (sqlite3-finalize (indirect-sqlite3-stmt** sqlite3-stmt**)))
+    (free-sqlite3-stmt* sqlite3-stmt**)
+    (display "\n"))
 
-; close the database
-(display "sqlite3-close-v2: ")
-(display (sqlite3-close-v2 (indirect-sqlite3** sqlite3**)))
-(free-sqlite3* sqlite3**)
-(display "\n")
+  ; select the customer row
+  (let ((sqlite3-stmt** (malloc-sqlite3-stmt*)))
+
+    ; prepare the sql statement
+    (display "sqlite3-prepare-v2: ")
+    (display (sqlite3-prepare-v2 (indirect-sqlite3** sqlite3**) "SELECT * FROM Customers;" -1 sqlite3-stmt** #f))
+    (display "\n")
+
+    ; execute the sql statement
+    (display "sqlite3-step: ")
+    (display (sqlite3-step (indirect-sqlite3-stmt** sqlite3-stmt**)))
+    (display "\n")
+
+    ; get the number of returned columns
+    (display "sqlite3-column-count: ")
+    (define columns-count (sqlite3-column-count (indirect-sqlite3-stmt** sqlite3-stmt**)))
+    (display columns-count)
+    (display "\n")
+
+    ; get the types of the columns
+    (map
+      (lambda (column-index)
+        (display "sqlite3-column-type: ")
+        (display (sqlite3-column-type (indirect-sqlite3-stmt** sqlite3-stmt**) column-index))
+        (display "\n"))
+      (iota columns-count))
+
+    ; get the value of the integer column
+    (display "sqlite3-column-int: ")
+    (display (sqlite3-column-int (indirect-sqlite3-stmt** sqlite3-stmt**) 0))
+    (display "\n")
+
+    ; get the value of the double column
+    (display "sqlite3-column-double: ")
+    (display (sqlite3-column-double (indirect-sqlite3-stmt** sqlite3-stmt**) 1))
+    (display "\n")
+
+    ; get the value of the text column
+    (display "sqlite3-column-text: ")
+    (display (sqlite3-column-text (indirect-sqlite3-stmt** sqlite3-stmt**) 2))
+    (display "\n")
+
+    ; finalize the sql statement
+    (display "sqlite3-finalize: ")
+    (display (sqlite3-finalize (indirect-sqlite3-stmt** sqlite3-stmt**)))
+    (free-sqlite3-stmt* sqlite3-stmt**)
+    (display "\n"))
+
+  ; close the database
+  (display "sqlite3-close-v2: ")
+  (display (sqlite3-close-v2 (indirect-sqlite3** sqlite3**)))
+  (free-sqlite3* sqlite3**)
+  (display "\n"))
