9e2e4b08ef2170a1d1b1d887fa9f96856e6a8a3d
Mathieu Desrochers <mathieu@desrochers>
2012-11-18 15:39:27 -0500

    Added proper error handling to sql.scm

--- sources/infrastructure/sql/sql.scm
+++ sources/infrastructure/sql/sql.scm
@@ -24,7 +24,10 @@
         (abort exception))
       (let ((sqlite3-open-result (sqlite3-open database-name sqlite3**)))
         (unless (= sqlite3-open-result sqlite3-result-ok)
-          (abort (string-append "could not open database " database-name)))
+          (abort
+            (string-append
+              "could not open database "
+              database-name)))
         (let* ((sql-connection (make-sql-connection (indirect-sqlite3** sqlite3**)))
                (procedure-result (procedure sql-connection)))
             (close-sqlite3* sqlite3**)
@@ -37,7 +40,10 @@
       (lambda (sqlite3-stmt*)
         (let ((sqlite3-step-result (sqlite3-step sqlite3-stmt*)))
           (unless (= sqlite3-step-result sqlite3-result-done)
-            (abort (string-append "could not step statement " statement))))))))
+            (abort
+              (string-append
+                "could not step statement "
+                statement))))))))
 
 ;; executes a sql statement that returns rows
 (define (sql-read sql-connection statement . parameter-values)
@@ -62,7 +68,11 @@
              (sqlite3-bind-text sqlite3-stmt* parameter-number parameter-value -1 sqlite3-transient))
             ((not parameter-value)
              (sqlite3-bind-null sqlite3-stmt* parameter-number))
-            (else (abort "unsupported parameter type")))))
+            (else
+              (abort
+                (string-append
+                  "unsupported parameter value "
+                  (format "~a" parameter-value)))))))
   (map
     (lambda (parameter-index)
       (let ((sql-bind-parameter-result (sql-bind-parameter parameter-index)))
@@ -87,7 +97,10 @@
         (abort exception))
       (let ((sqlite3-prepare-v2-result (sqlite3-prepare-v2 sqlite3* statement -1 sqlite3-stmt** #f)))
         (unless (= sqlite3-prepare-v2-result sqlite3-result-ok)
-          (abort (string-append "could not prepare statement " statement)))
+          (abort
+            (string-append
+              "could not prepare statement "
+              statement)))
         (let ((sqlite3-stmt* (indirect-sqlite3-stmt** sqlite3-stmt**)))
           (sql-bind-parameters sqlite3-stmt* parameter-values)
           (let ((procedure-result (procedure sqlite3-stmt*)))
@@ -104,7 +117,11 @@
           ((= column-type sqlite3-type-text)
            (sqlite3-column-text sqlite3-stmt* column-index))
           ((= column-type sqlite3-type-null) #f)
-          (else (abort "unsupported column type")))))
+          (else
+            (abort
+              (string-append
+                "unsupported column type "
+                (number->string column-type)))))))
 
 ;; reads all the rows from a sql-stmt*
 (define (sql-read-all-rows statement sqlite3-stmt*)
@@ -118,7 +135,10 @@
       (let ((sqlite3-step-result (sqlite3-step sqlite3-stmt*)))
         (unless (or (= sqlite3-step-result sqlite3-result-row)
                     (= sqlite3-step-result sqlite3-result-done))
-          (abort (string-append "could not step statement" statement)))
+          (abort
+            (string-append
+              "could not step statement "
+              statement)))
         (if (= sqlite3-step-result sqlite3-result-row)
           (let ((row (sql-read-row sqlite3-stmt* columns-count)))
             (accumulate-rows (cons row rows)))
