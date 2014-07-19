225ab57b38a0a1fc49e36203e2a4a64b923e255b
Mathieu Desrochers <mathieu@desrochers>
2014-07-19 15:34:04 -0400

    Limit the dependencies to texlive-latex-base

--- sources/infrastructure/latex/latex.scm
+++ sources/infrastructure/latex/latex.scm
@@ -11,52 +11,41 @@
 ;; returns the pdf compiled from a latex source
 (define (latex-print-pdf latex-source)
 
-  ;; create a temporary file
-  (let ((temporary-file-path (create-temporary-file "tex")))
+  ;; get the temporary file paths
+  (let* ((temporary-tex-file-path (create-temporary-file "tex"))
+         (temporary-pdf-file-path (pathname-replace-extension temporary-tex-file-path "pdf"))
+         (temporary-directory (pathname-directory temporary-tex-file-path)))
 
-    ;; get the name of the other temporary files
-    (let ((temporary-dvi-file-path (pathname-replace-extension temporary-file-path "dvi"))
-          (temporary-pdf-file-path (pathname-replace-extension temporary-file-path "pdf"))
-          (temporary-directory (pathname-directory temporary-file-path)))
+    (with-guaranteed-release
+      (lambda ()
 
-      (with-guaranteed-release
-        (lambda ()
+        ;; open the temporary latex file
+        (file-open
+          temporary-tex-file-path
+          (+ open/wronly
+             open/append
+             open/creat)))
 
-          ;; open the temporary file
-          (file-open
-            temporary-file-path
-            (+ open/wronly
-               open/append
-               open/creat)))
+      (lambda (temporary-file-descriptor)
 
-        (lambda (temporary-file-descriptor)
+        ;; write the latex source to the temporary file
+        (file-write temporary-file-descriptor latex-source)
 
-          ;; write the latex source to the temporary file
-          (file-write temporary-file-descriptor latex-source)
+        ;; invoke the latex process
+        (process-wait
+          (process-run
+            "/usr/bin/pdflatex"
+            (list
+              "-interaction=batchmode"
+              (string-append "-output-directory=" temporary-directory)
+              temporary-tex-file-path)))
 
-          ;; invoke the latex process
-          (process-wait
-            (process-run
-              "/usr/bin/latex"
-              (list
-                "-interaction=batchmode"
-                (string-append "-output-directory=" temporary-directory)
-                temporary-file-path)))
+        ;; read the temporary pdf file
+        (string->blob
+          (read-all
+            temporary-pdf-file-path)))
 
-          ;; invoke the dvipdf process
-          (process-wait
-            (process-run
-              "/usr/bin/dvipdf"
-              (list
-                temporary-dvi-file-path
-                temporary-pdf-file-path)))
+      (lambda (temporary-file-descriptor)
 
-          ;; read the pdf file
-          (string->blob
-            (read-all
-              temporary-pdf-file-path)))
-
-        (lambda (temporary-file-descriptor)
-
-          ;; close the temporary file
-          (file-close temporary-file-descriptor))))))
+        ;; close the temporary file
+        (file-close temporary-file-descriptor)))))
